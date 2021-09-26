e74876a0abee6f5e214a52cbeb3dde12678f3365
renatomak <renato.mark.silva@gmail.com>
2021-09-26 17:20:47 -0300

    component Door #02

--- components/door.jsx
+++ components/door.jsx
@@ -1,11 +1,15 @@
-import styles from "../styles/door.module.css";
+import styles from '../styles/door.module.css';
 
 const Door = (props) => {
-    return (
+  return (
+    <div className={styles.area}>
+      <div className={styles.frame}>
         <div className={styles.door}>
-            <div className={styles.number}>3</div>
+          <div className={styles.number}>3</div>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default Door;
