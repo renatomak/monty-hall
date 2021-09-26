493111e7776e8d058abbb13249be46912da725cb
renatomak <renato.mark.silva@gmail.com>
2021-09-26 16:05:20 -0300

    component gift

---
+++ components/gift.js
@@ -0,0 +1,13 @@
+import styles from "../styles/gift.module.css"
+const Gift = () => {
+    return (
+        <div className={styles.gift}>
+            <div className={styles.cover}></div>
+            <div className={styles.body}></div>
+            <div className={styles.tieOne}></div>
+            <div className={styles.tieTwo}></div>
+        </div>
+    );
+}
+
+export default Gift;
